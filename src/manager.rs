a6f5a7eeaa7079b743a3f417744f67023d4c8910
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2025-01-08 21:56:59 +0330

    initial
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

---
+++ src/manager.rs
@@ -0,0 +1,52 @@
+use std::sync::{Arc, Mutex};
+use tokio::time::{sleep, Duration};
+use tonic::transport::{Channel, Error};
+
+use crate::stream::stream_service_client::StreamServiceClient;
+
+pub struct GrpcConnectionManager {
+    client: Arc<Mutex<Option<StreamServiceClient<Channel>>>>,
+    endpoint: String,
+}
+
+impl GrpcConnectionManager {
+    pub fn new(endpoint: String) -> Self {
+        Self {
+            client: Arc::new(Mutex::new(None)),
+            endpoint,
+        }
+    }
+
+    pub async fn connect(&self) -> Result<(), Error> {
+        let client = StreamServiceClient::connect(self.endpoint.clone()).await?;
+        let mut client_lock = self.client.lock().unwrap();
+        *client_lock = Some(client);
+        Ok(())
+    }
+
+    pub async fn get_client(&self) -> Result<StreamServiceClient<Channel>, Error> {
+        let client_option = {
+            let client_lock = self.client.lock().unwrap();
+            client_lock.clone()
+        };
+
+        if let Some(client) = client_option {
+            Ok(client)
+        } else {
+            self.connect().await?;
+            let client_lock = self.client.lock().unwrap();
+            Ok(client_lock.clone().unwrap())
+        }
+    }
+
+    pub async fn reconnect_with_backoff(&self) {
+        let mut delay = 1;
+        loop {
+            if self.connect().await.is_ok() {
+                break;
+            }
+            sleep(Duration::from_secs(delay)).await;
+            delay = (delay * 2).min(32);
+        }
+    }
+}
