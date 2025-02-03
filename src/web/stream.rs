06d74d520ef2ea901236c46bb6618a0c4ac71002
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2025-02-03 12:23:31 +0330

    feat: code status
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- src/web/stream.rs
+++ src/web/stream.rs
@@ -179,8 +179,8 @@ pub struct SendResponse {
     pub reason: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(string, optional, tag = "3")]
     pub room_id: ::core::option::Option<::prost::alloc::string::String>,
-    #[prost(int64, optional, tag = "4")]
-    pub stats_code: ::core::option::Option<i64>,
+    #[prost(int32, optional, tag = "4")]
+    pub stats_code: ::core::option::Option<i32>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ChatMessage {
